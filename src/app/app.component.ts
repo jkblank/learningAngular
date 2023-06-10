099f9746220215cba3dc4f396430c30214d0a2e1
JKlerck <28999142@student365.msfed.nwu.ac.za>
2023-06-10 15:05:06 +0200

    Basic component explanation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //can be used as a tag in html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learningAngular';
+  title = 'BigAssAngularApp';
+  message = 'Angular is cool!'
 }
